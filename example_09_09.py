93bf4c83a93e88b4043c0592d2e28e4915ef5bdc
Alex_Radon <svirel.org@mail.ru>
2022-04-14 16:34:28 +0300

    Виджеты Listbox и Scrollbar

---
+++ example_09_09.py
@@ -0,0 +1,45 @@
+"""
+Виджеты Listbox и Scrollbar
+
+Простой настраиваемый компонент окна списка с прокруткой
+Передавая различные списки выбора в аргументе options и
+переопределяя метод runCommand в подклассе, можно повторно
+использовать определенный здесь класс компонента ScrolledList
+всякий раз когда потребуется вывести список с прокруткой.
+"""
+from tkinter import *
+
+class ScrolledList(Frame):
+    def __init__(self, options, parent=None):
+        Frame.__init__(self, parent)
+        self.pack(expand=YES, fill=BOTH)            # сделать растягиваемым
+        self.makeWidgets(options)
+
+    def handleList(self, event):
+        index = self.listbox.curselection()         # при двойном щелчке на списке
+        label = self.listbox.get(index)             # извлечь выбранный текст
+        self.runCommand(label)                      # и вызвать действие или get(ACTIVE)
+
+    def makeWidgets(self, options):
+        sbar = Scrollbar(self)
+        list = Listbox(self, relief=SUNKEN)
+        sbar.config(command=list.yview)             # связать sbar и list
+        list.config(yscrollcommand=sbar.set)        # сдвиг одного = сдвиг другого
+        sbar.pack(side=RIGHT, fill=Y)               # первым добавлен - последним обрезан
+        list.pack(side=LEFT, expand=YES, fill=BOTH) # список обрезается первым
+        pos = 0
+        for label in options:                       # добавить в виджет списка
+            list.insert(pos, label)                 # или insert(END, label)
+            pos += 1                                # или enumerate(options)
+        # list.config(selectmode=SINGLE, setgrid=1) # режимы выбора, изменение размеров
+        list.bind('<Double-1>', self.handleList)    # установить обработчик события
+        self.listbox = list
+
+    def runCommand(self, selection):                # необходимо переопределить
+        print('You selected:', selection)
+
+if __name__ == '__main__':
+    options = (('Lumberjack-%s' % x) for x in range(20)) # или map/lambda,
+    ScrolledList(options).mainloop()                     # [...]
+
+
